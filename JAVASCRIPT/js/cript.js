1dfb69f3f8178482f0c691200f36892f76e6744f
Nangin <114378083+Foxamyth@users.noreply.github.com>
2022-09-26 12:06:32 +0700

    Add files via upload

---
+++ JAVASCRIPT/js/cript.js
@@ -0,0 +1,5 @@
+var tinggi = 25 ;
+var alas = 40 ;
+
+var hasil= 1/2 * tinggi * alas;
+
---
+++ JAVASCRIPT/js/script.js
@@ -0,0 +1,18 @@
+//  var nama="Yanto"
+//         document.write("Nama Saya:" +nama+ "<br>");
+//         var umur= "17 thn"
+//         document.write("Umur: " +umur); 
+// alert("Minta Duit Banh")
+
+
+// var ytb=confirm("Apakah Anda Ingin Pindah Negara?");
+
+// if(ytb){
+//     window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+// } else {
+//     document.write ("ok,anda manusia yang nggak tau mau ngapain ya:v")
+// }
+
+var nama= prompt(" who are you? ","");
+
+document.write(" Hello Madafaka " +nama+ " ya ");
